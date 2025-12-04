6aff547891c6a0fda16e31c525012dcf51c6db44
Aaryan Panigrahi <aaryan.panigrahii@gmail.com>
2025-12-04 17:45:28 -0500

    added endspace

--- source/flex_counter_dir.sv
+++ source/flex_counter_dir.sv
@@ -1,105 +1,130 @@
 `timescale 1ns / 10ps
 
-module flex_counter_dir #(
-    parameter SIZE = 4
+module pixel_pos #(
+    parameter X_MAX = 5,
+    parameter Y_MAX = 5
 ) (
-    input  logic                 clk,
-    input  logic                 n_rst,
-    input  logic                 clear,
-    input  logic                 count_enable,  
-    input  logic [SIZE-1:0]      wrap_val,
-    input  logic [1:0]           mode,           
-    output logic [SIZE-1:0]      count_out,
-    output logic                 rollover_flag,  
-    output logic                 wrap_flag       
+    input logic clk, n_rst,
+
+    input logic update_pos, new_trans,
+    input logic [$clog2(X_MAX) - 1:0] max_x,
+    input logic [$clog2(Y_MAX) - 1:0] max_y,
+    output logic end_pos, next_dir,
+    output logic [$clog2(X_MAX) - 1:0] curr_x, 
+    output logic [$clog2(Y_MAX) - 1:0] curr_y
 );
+    //input logic [1:0] dir;
 
-    typedef enum logic {DIR_UP, DIR_DOWN} dir_e;
-    dir_e dir_ff, dir_next;
+    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    
+    // Previous Value updates
+    logic rollover_flag, rollover_flag_prev, rollover_flag_prev_exp;
+    logic wrap_flag, wrap_flag_prev, wrap_flag_prev_exp;
 
-    logic [SIZE-1:0] count_out_next;
-    logic rollover_flag_next, wrap_flag_next;
-
-    // Direction logic and FFs
-    always_ff @(posedge clk, negedge n_rst) begin
+    always_ff @(posedge clk, negedge n_rst) begin : UPDATE_PREV
         if (!n_rst) begin
-            count_out     <= '0;
-            
-            dir_ff        <= DIR_UP;
-
-            rollover_flag <= 1'b0;
-            wrap_flag     <= 1'b0;  
+            rollover_flag_prev  <= 0;
+            wrap_flag_prev      <= 0;
         end
+
         else begin
-            count_out     <= count_out_next;
-
-            dir_ff        <= dir_next;
-
-            rollover_flag <= rollover_flag_next;
-            wrap_flag     <= wrap_flag_next;
+            rollover_flag_prev   <= rollover_flag_prev_exp;
+            wrap_flag_prev       <= wrap_flag_prev_exp;
         end
     end
 
-    // NS Logic based on mode
-    always_comb begin
-        count_out_next = count_out;
-        dir_next       = dir_ff;
+    always_comb begin : UPDATE_PREV_EXP
+        rollover_flag_prev_exp  = rollover_flag_prev;
+        wrap_flag_prev_exp      = wrap_flag_prev;
 
-        if (clear) begin
-            count_out_next = '0;
-            dir_next       = DIR_UP;
-        end else if (count_enable) begin
-            unique case (mode)
-                // 1 - wrap_val jump 1
-                2'b00: begin 
-                    if (count_out >= wrap_val)
-                        count_out_next = {{(SIZE-1){1'b0}}, 1'b1};
-                    else
-                        count_out_next = count_out + 1;
-                end
-
-                // wrap_val - 1, jump wrap_val
-                2'b01: begin 
-                    if (count_out <= {{(SIZE-1){1'b0}}, 1'b1})
-                        count_out_next = wrap_val;
-                    else
-                        count_out_next = count_out - 1;
-                end
-
-                // 0 - wrap_val - 0
-                2'b10: begin 
-                    unique case (dir_ff)
-                        DIR_UP: begin
-                            if (count_out >= wrap_val) begin
-                                count_out_next = wrap_val - 1;
-                                dir_next       = DIR_DOWN;
-                            end else begin
-                                count_out_next = count_out + 1;
-                            end
-                        end
-                        DIR_DOWN: begin
-                            if (count_out == '0) begin
-                                count_out_next = 1;
-                                dir_next       = DIR_UP;
-                            end else begin
-                                count_out_next = count_out - 1;
-                            end
-                        end
-                    endcase
-                end
-
-                // 2'b11: hold
-                default: begin 
-                    count_out_next = count_out;
-                end
-            endcase
+        if (update_pos) begin
+            rollover_flag_prev_exp  = rollover_flag;
+            wrap_flag_prev_exp      = wrap_flag;
         end
     end
 
-    // Output Flag
-    always_comb begin
-        wrap_flag_next     = (count_out_next == wrap_val);
-        rollover_flag_next = (count_out_next == '0);
+    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////
+    logic new_trans_prev, new_trans_prev_exp;
+
+    always_ff @(posedge clk, negedge n_rst) begin : UPDATE_TRANS_PREV
+        if (!n_rst) begin
+            new_trans_prev  <= 0;
+        end
+
+        else begin
+            new_trans_prev  <= new_trans_prev_exp;
+        end
     end
+
+    always_comb begin : UPDATE_TRANS_PREV_EXP
+        new_trans_prev_exp  = new_trans_prev;
+
+        if (update_pos) begin
+            new_trans_prev_exp  = new_trans;
+        end
+    end
+
+    logic new_trans_info;
+    assign new_trans_info = new_trans || new_trans_prev;
+    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    
+
+    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    
+    logic new_flag, old_flag;
+
+    always_comb begin : FLAG_INFO
+        new_flag = wrap_flag || rollover_flag;
+        old_flag = wrap_flag_prev || rollover_flag_prev;
+    end
+
+    logic y_update, x_update;
+
+    always_comb begin : MODULE_UPDATES
+        y_update = update_pos && (new_flag && !old_flag) && !new_trans_info;
+        x_update = update_pos && (new_trans_info || !(new_flag && !old_flag));
+    end
+    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    //// 
+
+    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////
+    logic [$clog2(X_MAX) - 1:0] max_x_eff;
+    logic [$clog2(Y_MAX) - 1:0] max_y_eff;
+
+    assign max_x_eff = max_x - 1;
+    assign max_y_eff = max_y - 1;
+    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////   
+
+    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    //// 
+    logic end_x, end_y;
+
+    always_comb begin : END_DATA
+        end_x = (max_x_eff == curr_x);
+        end_y = (max_y_eff == curr_y);
+
+        end_pos = end_x && end_y;
+    end
+
+    always_comb begin : DIRECTION_INFO
+        next_dir = y_update;
+    end
+    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    //// 
+
+    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    //// 
+    logic corr_clear;
+
+    assign corr_clear = new_trans || end_pos; 
+    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    //// 
+
+    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////
+    localparam X_MODE = 2'd2;      // UP-DOWN
+    localparam Y_MODE = 2'd0;      // COUNT-UP
+
+    flex_counter_dir #(.SIZE($clog2(X_MAX))) X_CORR 
+                            (.clk(clk), .n_rst(n_rst),
+                            .count_enable(x_update), .wrap_val(max_x_eff), .mode(X_MODE), .clear(corr_clear),
+                            .count_out(curr_x), .wrap_flag(wrap_flag), .rollover_flag(rollover_flag));
+
+    flex_counter_dir #(.SIZE($clog2(Y_MAX))) Y_CORR 
+                            (.clk(clk), .n_rst(n_rst),
+                            .count_enable(y_update), .wrap_val(max_y_eff), .mode(Y_MODE), .clear(corr_clear),
+                            .count_out(curr_y), .wrap_flag(), .rollover_flag());
+    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////
 
 endmodule
