f1193b8ca2eb920b3c49a61af7f9c4985b8d5bd6
Aaryan Panigrahi <aaryan.panigrahii@gmail.com>
2025-12-07 20:30:36 -0500

    fixed image loading

--- source/sram_image.sv
+++ source/sram_image.sv
@@ -24,7 +24,7 @@ module sram_image #(
 
     ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////
     // Map 1D SRAM as 2D SRAM
-    logic [$clog2(X_MAX) * $clog2(Y_MAX) - 1:0] corr_addr;
+    logic [ADDR_WIDTH - 1:0] corr_addr;
     assign corr_addr = x_addr + (y_addr * X_MAX);
     ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////
 
@@ -38,43 +38,46 @@ module sram_image #(
     logic [PIXEL_DEPTH-1:0] sram_rdat;
     always_comb begin
             rdat = sram_rdat;
-            if (ren_prev && ((x_addr > x_max_eff) || (y_addr > y_max_eff))) rdat = '0;
+            
+            if (ren_prev) begin
+                // Out of Bounds
+                if ((x_addr > x_max_eff) || (y_addr > y_max_eff)) rdat = '0;
+            end
     end
 
     sram_model #(.ADDR_WIDTH(ADDR_WIDTH), .DATA_WIDTH(PIXEL_DEPTH), .RAM_IS_SYNCHRONOUS(1)) IMAGE_DUT (
         .ramclk(ramclk), .addr(corr_addr), .wen(wen), .ren(ren), 
         .wdat(wdat), 
         .rdat(sram_rdat)
-    );
+    );                                                                     
     ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    ////
 
     ////    ////    ////    ////    ////    ////    ////    ////    ////    //// 
     // Inside sram_image
     function automatic void load_img(input string fname,
-                                    input int xdim,
-                                    input int ydim);
+                                input int xdim,
+                                input int ydim);
         automatic bit [PIXEL_DEPTH-1:0] temp_img []; 
         automatic int total_pixels;
+        automatic int ram_idx;
         automatic int px_idx;
         automatic int image_idx;
         
         total_pixels = xdim * ydim;
-
         temp_img = new[total_pixels];
-
         $readmemh(fname, temp_img);
 
-        // 2) Clear entire underlying SRAM
+        // Clear entire SRAM
         for (int i = 0; i < (1 << ADDR_WIDTH); i++) begin
             IMAGE_DUT.ram[i] = '0;
         end
 
         for (int y = 0; y < ydim; y++) begin
             for (int x = 0; x < xdim; x++) begin
-                px_idx = x + (y * X_MAX); 
-                image_idx = x + (y * xdim);
+                ram_idx = x + (y * X_MAX);
+                px_idx = x + (y * xdim);
                 if (px_idx < total_pixels)
-                    IMAGE_DUT.ram[px_idx] = temp_img[image_idx];
+                    IMAGE_DUT.ram[ram_idx] = temp_img[px_idx];
             end
         end
     endfunction
