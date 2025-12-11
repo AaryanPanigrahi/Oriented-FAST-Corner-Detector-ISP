56d3037449e883f821a708ad77c6bafa2126c742
Aaryan Panigrahi <aaryan.panigrahii@gmail.com>
2025-12-11 00:53:16 -0500

    addrd dual port

--- source/sram_model.sv
+++ source/sram_model.sv
@@ -17,10 +17,12 @@ Date: 11/18/2025
 module sram_model #(
     parameter ADDR_WIDTH = 18,
     parameter DATA_WIDTH = 32,
-    parameter RAM_IS_SYNCHRONOUS = 1
+    parameter RAM_IS_SYNCHRONOUS = 1,
+    parameter DUAL = 0
 )(
     input logic ramclk,                     // Acts as en/dis (if async) - no reset
     input logic [ADDR_WIDTH-1:0] addr,
+    input logic [ADDR_WIDTH-1:0] addr_write,
     input logic wen, ren,
     input logic [DATA_WIDTH-1:0] wdat,
     output logic [DATA_WIDTH-1:0] rdat
@@ -29,22 +31,37 @@ module sram_model #(
 bit [DATA_WIDTH-1:0] ram [2**ADDR_WIDTH]; 
 
 ////    ////    ////    ////    ////    ////    ////    ////    ////    //// 
-// WRITE
+// WRITE - if mode then uses addr_write
 always_ff @(posedge ramclk) begin : WRITE
-    if (wen) ram[addr] <= wdat;
+    if (wen) begin
+        if (DUAL)   ram[addr_write] <= wdat;
+        else        ram[addr]       <= wdat;
+    end
 end
 
-// READ
+// READ - always uses addr
 generate
     if (RAM_IS_SYNCHRONOUS) begin : READ_SYNC
         always_ff @(posedge ramclk) begin : READ_SYNC_FF
-            rdat <= (ren) ? ram[addr] : 'x;
+            if (ren) begin
+                if (addr == {ADDR_WIDTH{1'b1}})   // check if address is all ones (max value)
+                    rdat <= '0;                  // return 0
+                // RAW handler - write addr and read addr same
+                else if (DUAL && (addr_write == addr))
+                    rdat <= wdat;
+                else
+                    rdat <= ram[addr];
+            end
+            else begin
+                rdat <= 'x;
+            end
         end
     end
     else begin : READ_ASYNC
-        assign rdat = (ren) ? ram[addr] : 'x;
+        assign rdat = (ren) ? ((addr == {ADDR_WIDTH{1'b1}}) ? '0 : ram[addr]) : 'x;
     end
 endgenerate
+
 ////    ////    ////    ////    ////    ////    ////    ////    ////    ////   
 
 ////    ////    ////    ////    ////    ////    ////    ////    ////    ////    
